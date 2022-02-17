f27e27cfe2cfeeebcf1dae2156ff18b30b376f4b
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-17 14:18:40 -0500

    UsuarioController metodos obtener y guardar usuario

--- src/main/java/com/example/demo/controllers/UsuarioController.java
+++ src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -1,4 +1,25 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.UsuarioModel;
+import com.example.demo.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping()
+    public ArrayList<UsuarioModel> obtenerUsuario () {
+        return usuarioService.obtenerUsuario();
+    }
+
+    @PostMapping()
+    public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario) {
+        return this.usuarioService.guardarUsuario(usuario);
+    }
 }
