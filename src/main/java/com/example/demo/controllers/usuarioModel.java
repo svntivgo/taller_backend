ac27650e95710e2d1219405a7107196421fe4c13
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-17 13:37:39 -0500

    add usuarioModel getters setters

--- src/main/java/com/example/demo/controllers/usuarioModel.java
+++ src/main/java/com/example/demo/controllers/usuarioModel.java
@@ -1,16 +1,49 @@
 package com.example.demo.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import javax.persistence.*;
 
-@RestController
+@Entity
+@Table(name = "usuario")
 public class usuarioModel {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(unique = true, nullable = false)
     private Long id;
     private String nombre;
     private String mail;
     private  Integer prioridad;
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setPrioridad(Integer prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public Integer getPrioridad() {
+        return prioridad;
+    }
 
 }
