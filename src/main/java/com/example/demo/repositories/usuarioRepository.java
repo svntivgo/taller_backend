5245d8eb4aa70fe95bf5a2b2fb9ba5642628d7e1
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-17 13:46:57 -0500

    se extiende de CrudRepository

--- src/main/java/com/example/demo/repositories/usuarioRepository.java
+++ src/main/java/com/example/demo/repositories/usuarioRepository.java
@@ -1,4 +1,10 @@
 package com.example.demo.repositories;
 
-public class usuarioRepository {
+import com.example.demo.models.usuarioModel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface usuarioRepository extends CrudRepository<usuarioModel, Long> {
+
 }
