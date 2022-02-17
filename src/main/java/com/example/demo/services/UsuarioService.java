71747111dda149552a633ce857b7af9b915f20c7
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-17 16:13:21 -0500

    ObtenerPorId ObtenerPorPrioridad eliminarUsuario

--- src/main/java/com/example/demo/services/UsuarioService.java
+++ src/main/java/com/example/demo/services/UsuarioService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -19,6 +20,23 @@ public class UsuarioService {
 
     public UsuarioModel guardarUsuario (UsuarioModel usuario) {
         return usuarioRepository.save(usuario);
+    }
+
+    public Optional<UsuarioModel> obtenerPorId (Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public ArrayList<UsuarioModel> obtenerPorPrioridad (Integer prioridad) {
+        return usuarioRepository.findByPrioridad(prioridad);
+    }
+
+    public Boolean eliminarUsuario (Long id) {
+        try {
+            usuarioRepository.deleteById(id);
+            return true;
+        } catch (Exception err) {
+            return false;
+        }
     }
 
 
