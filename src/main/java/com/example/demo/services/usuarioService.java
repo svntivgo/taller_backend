62f8470215734793acc516370c53ba6813818f05
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-17 13:59:51 -0500

    llamado a usuarioRepository en servicio

--- src/main/java/com/example/demo/services/usuarioService.java
+++ src/main/java/com/example/demo/services/usuarioService.java
@@ -1,4 +1,12 @@
 package com.example.demo.services;
 
+import com.example.demo.repositories.usuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class usuarioService {
+
+    @Autowired
+    usuarioRepository usuarioRepository;
 }
