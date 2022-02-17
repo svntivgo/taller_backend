eba26f0b2d899f0a7a8d7f081d831ae3f177602e
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-17 13:14:36 -0500

    se crea usuarioModel

---
+++ src/main/java/com/example/demo/controllers/usuarioModel.java
@@ -0,0 +1,16 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class usuarioModel {
+
+
+    private Long id;
+    private String nombre;
+    private String mail;
+    private  Integer prioridad;
+
+
+}
