1c4fc587271abf6ab6abc69955b8e6969ea9964d
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-17 23:07:11 -0500

    Comentarios tipo JSDoc

--- src/main/java/com/example/demo/controllers/UsuarioController.java
+++ src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -14,26 +14,50 @@ public class UsuarioController {
     @Autowired
     UsuarioService usuarioService;
 
+    /**
+     * Obtiene todos los usuario de la DB
+     * @return JSON de todos los usuarios
+     */
     @GetMapping()
     public ArrayList<UsuarioModel> obtenerUsuario () {
         return usuarioService.obtenerUsuario();
     }
 
+    /**
+     * Guarda un usuario en la DB
+     * @param usuario JSON con la información del usuario
+     * @return JSON con la información del usuario
+     */
     @PostMapping()
     public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario) {
         return this.usuarioService.guardarUsuario(usuario);
     }
 
+    /**
+     * Obtiene un usuario de la DB por su ID
+     * @param id del usuario
+     * @return JSON con la información del usuario
+     */
     @GetMapping(path = "/{id}")
     public Optional<UsuarioModel> obtenerUsuarioPorId (@PathVariable("id") Long id) {
         return this.usuarioService.obtenerPorId(id);
     }
 
+    /**
+     * Obtiene una lista de usuarios por su prioridad
+     * @param prioridad
+     * @return JSON con la información de los usuarios
+     */
     @GetMapping("/query")
     public ArrayList<UsuarioModel> obtenerUsuarioPorPrioridad (@RequestParam("prioridad") Integer prioridad) {
         return this.usuarioService.obtenerPorPrioridad(prioridad);
     }
 
+    /**
+     * Elimina un usuario por su ID
+     * @param id
+     * @return Mensaje de resultado
+     */
     @DeleteMapping(path = "/{id}")
     public String eliminarPorId (@PathVariable("id") Long id) {
         Boolean ok = this.usuarioService.eliminarUsuario(id);
@@ -44,6 +68,10 @@ public class UsuarioController {
         }
     }
 
+    /**
+     * Elimina todos los usuarios
+     * @return Mensaje de resultado
+     */
     @DeleteMapping(path = "/eliminar-todos")
     public String eliminarTodos () {
         Boolean ok = this.usuarioService.eliminarTodosLosUsuarios();
@@ -54,6 +82,11 @@ public class UsuarioController {
         }
     }
 
+    /**
+     * Obtiene un usuario por su nombre
+     * @param nombre
+     * @return JSON con la información del usuario
+     */
     @GetMapping(path = "/q")
     public ArrayList<UsuarioModel> obtenerUsuarioPornombre (@RequestParam("nombre") String nombre) {
         return this.usuarioService.obtenerPorNombre(nombre);
--- src/main/java/com/example/demo/services/UsuarioService.java
+++ src/main/java/com/example/demo/services/UsuarioService.java
@@ -15,26 +15,32 @@ public class UsuarioService {
     UsuarioRepository usuarioRepository;
 
     public ArrayList<UsuarioModel> obtenerUsuario () {
+
         return (ArrayList<UsuarioModel>) usuarioRepository.findAll();
     }
 
     public ArrayList<UsuarioModel> obtenerPorNombre (String nombre) {
+
         return usuarioRepository.findByNombre(nombre);
     }
 
     public UsuarioModel guardarUsuario (UsuarioModel usuario) {
+
         return usuarioRepository.save(usuario);
     }
 
     public Optional<UsuarioModel> obtenerPorId (Long id) {
+
         return usuarioRepository.findById(id);
     }
 
     public ArrayList<UsuarioModel> obtenerPorPrioridad (Integer prioridad) {
+
         return usuarioRepository.findByPrioridad(prioridad);
     }
 
     public Boolean eliminarUsuario (Long id) {
+
         try {
             usuarioRepository.deleteById(id);
             return true;
@@ -44,6 +50,7 @@ public class UsuarioService {
     }
 
     public Boolean eliminarTodosLosUsuarios () {
+
         try {
             usuarioRepository.deleteAll();
             return true;
